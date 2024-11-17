f91810ad32dcc28610bc9fde9598d02e78a18ba4
PX <120547170+Kileer20100@users.noreply.github.com>
2024-11-17 14:02:56 +0200

    Delete client2 directory

--- client2/get2.py
+++
@@ -1,9 +0,0 @@
-import zmq
-
-context = zmq.Context()
-socket = context.socket(zmq.DEALER)
-socket.connect("tcp://localhost:5050")
-
-while True:
-    response = socket.recv()
-    print("Received from server:", response.decode())
